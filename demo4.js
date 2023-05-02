57faa0d6f299e0adbd91328bc97cc0f8c00b4d9c
kevinelong <kevinelong@gmail.com>
2023-05-02 08:21:46 -0700

    parse names fixed

--- demo4.js
+++ demo4.js
@@ -6,20 +6,19 @@
 function showName(name){
     let output = "";
     const SPACE = " ";
-
     const indexFirstSpace = name.indexOf(SPACE)
     if(indexFirstSpace === -1){
         output = name; //only one so use it all
     }else{
-        const indexSecondSpace = name.indexOf(SPACE, indexFirstSpace);
+        const indexSecondSpace = name.indexOf(SPACE, indexFirstSpace+1);
         if(indexSecondSpace === - 1){ //Only Two words
             const part1 = name.substring(0,indexFirstSpace)
             const part2 = name.substring(indexFirstSpace + 1)
             output = `${part2}, ${part1}`;
         }else{ //three
-            const indexThirdSpace = name.indexOf(SPACE, indexSecondSpace);
+            const indexThirdSpace = name.indexOf(SPACE, indexSecondSpace+1);
             const part1 = name.substring(0, indexFirstSpace)
-            const part2 = name.substring(indexFirstSpace + 1, indexSecondSpace)
+            const part2 = name.substring(indexFirstSpace+1, indexSecondSpace)
             const part3 = name.substring(indexSecondSpace + 1)
             output = `${part3}, ${part1} ${part2}`;
         }

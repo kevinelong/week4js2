5c5693e7907cef096c97bce301a5117df4b8dccb
kevinelong <kevinelong@gmail.com>
2023-05-03 11:03:40 -0700

    radio js and pseudocode

---
+++ demo_9_pseudocode.js
@@ -0,0 +1,15 @@
+// PSEUDO-CODE
+// 1. get text input from the user
+
+// 2. convert text to date object
+
+// 3. output formated local date
+
+// if the date is in the past
+// then show too late message
+
+// else if date is in the future
+// show count down message
+
+// else - must be today
+// show celebration message
